94516e6092052be50c16f28a765e8206c7b1a448
JosephDuvvani <josephduvvani@gmail.com>
2025-02-18 16:33:03 +0200

    Create sidebar, tables, and style the content
    Also style the categories

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ import express from "express";
 import { productCategoriesGet } from "./controllers/invControllers.js";
 import { configDotenv } from "dotenv";
 import { categoryRouter } from "./routes/categoryRouter.js";
+import path from "path";
+
 configDotenv();
 
 const app = express();
 
+const __dirname = path.resolve();
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.set("views", "views");
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
 app.get("/", productCategoriesGet);
 
